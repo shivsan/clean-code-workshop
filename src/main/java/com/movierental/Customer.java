49a1886f63be16156ed032c01ea3cd619905d9e0
Shivku <sivakumar@finzy.com>
2019-06-03 01:16:00 +0530

    Added htmlStatement function

--- src/main/java/com/movierental/Customer.java
+++ src/main/java/com/movierental/Customer.java
@@ -4,47 +4,62 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Customer {
-  private String name;
-  private List<Rental> rentals = new ArrayList<>();
+    private String name;
+    private List<Rental> rentals = new ArrayList<>();
 
-  public Customer(String name) {
-    this.name = name;
-  }
-
-  public void addRental(Rental arg) {
-    rentals.add(arg);
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public String statement() {
-    double totalAmount = 0;
-    String result = "Rental Record for " + getName() + "\n";
-    for (Rental each : rentals) {
-      double thisAmount = each.getAmount();
-
-      //show figures for this rental
-      result += "\t" + each.getMovie().getTitle() + "\t" +
-          String.valueOf(thisAmount) + "\n";
-      totalAmount += thisAmount;
+    public Customer(String name) {
+        this.name = name;
     }
 
-    //add footer lines result
-    result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-    result += "You earned " + getAllFrequenterRentalPoints()
-        + " frequent renter points";
-    return result;
-  }
+    public void addRental(Rental arg) {
+        rentals.add(arg);
+    }
 
-  public int getAllFrequenterRentalPoints(){
-      return rentals.stream().mapToInt(r->r.getFrequentRenterPoints()).sum();
-  }
+    public String getName() {
+        return name;
+    }
 
-  public String htmlStatement(){
-    return "";
-  }
+    public String statement() {
+        String result = "Rental Record for " + getName() + "\n";
+        for (Rental each : rentals) {
+            double thisAmount = each.getAmount();
 
+            //show figures for this rental
+            result += "\t" + each.getMovie().getTitle() + "\t" +
+                    String.valueOf(thisAmount) + "\n";
+        }
+
+        //add footer lines result
+        result += "Amount owed is " + String.valueOf(getTotalAmount()) + "\n";
+        result += "You earned " + getAllFrequentRentalPoints()
+                + " frequent renter points";
+        return result;
+    }
+
+    public int getAllFrequentRentalPoints() {
+        return rentals.stream().mapToInt(r -> r.getFrequentRenterPoints()).sum();
+    }
+
+    public String htmlStatement() {
+        String result = "Rental Record for <b>" + getName() + "</b><br>";
+        for (Rental each : rentals) {
+            double thisAmount = each.getAmount();
+
+            //show figures for this rental
+            result += "\t" + each.getMovie().getTitle() + "\t" +
+                    String.valueOf(thisAmount) + "<br>";
+        }
+
+        //add footer lines result
+        result += "Amount owed is <b>" + String.valueOf(getTotalAmount()) + "</b><br>";
+        result += "You earned <b>" + getAllFrequentRentalPoints()
+                + "</b> frequent renter points";
+        return result;
+    }
+
+
+    private double getTotalAmount() {
+        return rentals.stream().mapToDouble(r -> r.getAmount()).sum();
+    }
 }
 
